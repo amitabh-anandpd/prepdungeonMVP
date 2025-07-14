e59fecb4fcf454a8ef06263dee833dfa120ee67e
AMITABH ANAND <168374056+amitabh-anandpd@users.noreply.github.com>
2025-07-15 00:55:35 +0530

    Update settings.py

--- prepdungeon/settings.py
+++ prepdungeon/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,10 +130,9 @@ CSRF_COOKIE_SECURE = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = r"C:\Users\amita\Desktop\PrepDungeon\prepdungeonDjango\prepdungeon\media"
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
