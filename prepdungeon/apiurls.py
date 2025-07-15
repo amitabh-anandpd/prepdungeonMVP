6d8335e771443d54cf1d1b23eaa26bd965dcd5de
AMITABH ANAND <168374056+amitabh-anandpd@users.noreply.github.com>
2025-07-15 09:38:43 +0530

    Delete prepdungeon/apiurls.py

--- prepdungeon/apiurls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .apiviews import login_api, signup_api
-
-urlpatterns = [
-    path('login/', login_api, name='login_api'),
-    path('signup/', signup_api, name='signup_api'),
-]
