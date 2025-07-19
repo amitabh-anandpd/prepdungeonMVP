008d91d3784e34e401865ade57a7fe79569960f5
AMITABH ANAND <168374056+amitabh-anandpd@users.noreply.github.com>
2025-07-19 22:15:13 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+# exit on error
+set -o errexit
+
+# Tesseract
+echo "---> Installing Tesseract OCR Engine..."
+apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
+
+echo "---> Installing Python requirements..."
+pip install -r requirements.txt
+
+echo "---> Running Django database migrations..."
+python manage.py migrate
