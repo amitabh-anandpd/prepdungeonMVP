238b6bbfcc03f02133f29e39414eb7ad67062754
AMITABH ANAND <168374056+amitabh-anandpd@users.noreply.github.com>
2025-07-19 22:47:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ set -o errexit
 
 # Tesseract
 echo "---> Installing Tesseract OCR Engine..."
-apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
+#apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
+apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-eng
 
 echo "---> Installing Python requirements..."
 pip install -r requirements.txt
