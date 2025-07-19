faffe60f7dc895a79f83ed5ad0775f2eb1ee4af8
AMITABH ANAND <168374056+amitabh-anandpd@users.noreply.github.com>
2025-07-19 22:35:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ apt-get update && apt-get install -y tesseract-ocr libtesseract-dev
 echo "---> Installing Python requirements..."
 pip install -r requirements.txt
 
+echo "---> Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "---> Running Django database migrations..."
 python manage.py makemigrations
 python manage.py migrate
